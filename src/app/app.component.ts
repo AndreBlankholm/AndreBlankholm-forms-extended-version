865a47d9c617b6510a81147ff759a5b5cbbb7baa
AndreBlankholm <ablankholm@gmail.com>
2024-02-07 12:09:35 -0600

    adding form divs, options and form-controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'forms-extended-version';
+
+  projectForm! : FormGroup
+
+ngOnInit(): void {
+  this.projectForm = new FormGroup({
+    'projectName': new FormControl(null, Validators.required)
+  });
+}
+
+submit(){
+console.log("Submit has been clicked");
+}
+
 }
