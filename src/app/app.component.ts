e664c78b4a150b211fb0a1c6adfb1f8dea3182cb
AndreBlankholm <ablankholm@gmail.com>
2024-02-13 12:01:46 -0600

    adding validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
+import {CustomrValidators} from '../app/customValidators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'forms-extended-version';
 
-  projectForm! : FormGroup
+  projectForm!: FormGroup;
 
-ngOnInit(): void {
-  this.projectForm = new FormGroup({
-    'projectName': new FormControl(null, Validators.required),
-    'email': new FormControl(null, [Validators.required, Validators.email]),
-    'projectStatus' : new FormControl('critical')
-  });
-}
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl(null, [Validators.required]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl('critical'),
+    });
+  }
 
-submit(){
-console.log("Submit has been clicked");
-}
+  submit() {
+    console.log('Submit has been clicked');
+  }
 
-onSaveProject(){
-  console.log(this.projectForm.value);
-  console.log(this.projectForm);
-}
-
+  onSaveProject() {
+    console.log(this.projectForm.value);
+    console.log(this.projectForm);
+  }
 }
